7bf20d142cafcb5870799f787f79059195f106d3
Daniel Gabai <gabaidaniel0@gmail.com>
2023-12-21 11:57:03 +0100

    Tensorboard graphs

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
-import random
-import pandas as pd
-from stable_baselines3 import DQN, PPO, A2C
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.results_plotter import plot_results, load_results
-from gymnasium.wrappers import NormalizeObservation, NormalizeReward
-import random
-from pandas import Timestamp
-import warnings
-
+from stable_baselines3 import PPO
+from gymnasium.wrappers import NormalizeObservation
 from flight_scheduling import FlightSchedulingEnv
 from utils import generate_random_flight_schedule, generate_lambdas
 
-warnings.filterwarnings('ignore')
 
-random_schedule = generate_random_flight_schedule(10)
-random_lambdas = generate_lambdas(random_schedule)
+def main():
+    random_schedule = generate_random_flight_schedule(10)
+    random_lambdas = generate_lambdas(random_schedule)
 
-env = FlightSchedulingEnv(
-    flight_schedule=random_schedule,
-    lambdas=random_lambdas,
-    max_steps=1000,
-    revenue_estimation='classic'
-)
-env = NormalizeObservation(env)
-#env = NormalizeReward(env)
+    env = FlightSchedulingEnv(
+        flight_schedule=random_schedule,
+        lambdas=random_lambdas,
+        max_steps=100,
+        revenue_estimation='classic'
+    )
+    env = NormalizeObservation(env)
 
-model = PPO("MlpPolicy", env).learn(total_timesteps=10)
+    model = PPO("MlpPolicy", env).learn(total_timesteps=100000)
 
-for _ in range(1):
-    obs, _ = env.reset()
-    env.renderer()
-    total_reward = 0
-    done = False
-    count = 0
-    while not done:
-        action, _ = model.predict(obs)
-        obs, reward, done, _, _ = env.step(action)
-        #print('action : ', action)
-        #print('obs : ', obs)
-        #print('reward : ', reward)
-        total_reward += reward
-        env.renderer(init=False)
-    print(total_reward)
-env.close()
+    for _ in range(1):
+        obs, _ = env.reset()
+        env.renderer()
+        total_reward = 0
+        done = False
+        while not done:
+            action, _ = model.predict(obs)
+            obs, reward, done, _, _ = env.step(action)
+            total_reward += reward
+            env.renderer(init=False)
+        print(total_reward)
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
